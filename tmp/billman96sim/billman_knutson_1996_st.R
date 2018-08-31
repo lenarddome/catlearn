e1ecda810513aed172f23c93bd87a3a2b7b07726
Lenard Dome <lenarddome@protonmail.ch>
2018-08-31 14:57:44 +0100

    added range of temporary dev files and update billmanknutson

---
+++ tmp/billman96sim/billman_knutson_1996_st.R
@@ -0,0 +1,14 @@
+# Initial Parameters for SUSTAIN for Billman and Knutson (1996)
+# as reported in Love, B.C., Medin, D.L, and Gureckis, T.M (2004)
+# SUSTAIN: A Network Model of Category Learning.
+# Psychological Review, 11, 309-332.
+
+st <- list(r = 9.998779,
+           beta = 6.396300,
+           d = 1.977312,
+           eta = 0.096564,
+           tau = 0.5,
+           lambda = 1.0,
+           dims = rep(3, 7),
+           w = matrix(rep(0, 21), nrow = 1),
+           colskip = 2)
